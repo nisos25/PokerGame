94ce3d41915df7b651ac5fd8fe646ca62ec721b9
Nicolas Santander <josess.nicolas@hotmail.com>
2020-10-11 23:37:56 -0500

    Card generator

---
+++ PokerGame/Assets/Scripts/Card.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public enum Suit
+{
+    Club,
+    Spade,
+    Diamond,
+    Heart
+}
+
+public enum Value
+{
+    Two = 2,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven, 
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace
+}
+
+[Serializable]
+public class Card
+{
+    public Card(Suit suit, Value value)
+    {
+        CardSuit = suit;
+        CardValue = value;
+    }
+    
+    [field: SerializeField] public Suit CardSuit { get; set; }
+    [field: SerializeField] public Value CardValue { get; set; }
+}
---
+++ PokerGame/Assets/Scripts/RandomCardPicker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomCardPicker : MonoBehaviour
+{
+    [SerializeField]private Deck deck;
+    
+    private List<Card> playerCards = new List<Card>();
+    private List<Card> exchangeCards = new List<Card>();
+
+    private void Start()
+    {
+        GetExchangeCards();
+        GetFiveCardsDeck();
+    }
+
+    private void GetFiveCardsDeck()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            int cardPosition = Random.Range(0, deck.Cards.Count);
+            playerCards.Add(deck.Cards[cardPosition]);
+            deck.Cards.RemoveAt(cardPosition);
+        }
+    }
+
+    private void GetExchangeCards()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            int cardPosition = Random.Range(0, deck.Cards.Count);
+            exchangeCards.Add(deck.Cards[cardPosition]);
+            deck.Cards.RemoveAt(cardPosition);
+        }        
+    }
+}
