a745496a9bf921d2e56b3e5c7c8aa8e8ac65996b
Nicolas Santander <josess.nicolas@hotmail.com>
2020-10-13 02:41:18 -0500

    Assets graficos y codigo base

--- PokerGame/Assets/Scripts/RandomCardPicker.cs
+++ PokerGame/Assets/Scripts/RandomCardPicker.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class RandomCardPicker : MonoBehaviour
 {
     [SerializeField]private Deck deck;
-    
+    [SerializeField]private PlayerStates playerStates;
+    [SerializeField]private GameEvent changeCards;
+
     private List<Card> playerCards = new List<Card>();
     private List<Card> exchangeCards = new List<Card>();
 
@@ -17,17 +20,20 @@ public class RandomCardPicker : MonoBehaviour
 
     private void GetFiveCardsDeck()
     {
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < 5; i++)
         {
             int cardPosition = Random.Range(0, deck.Cards.Count);
             playerCards.Add(deck.Cards[cardPosition]);
             deck.Cards.RemoveAt(cardPosition);
         }
+
+        playerStates.CurrentPlayerDeck = playerCards;
+        changeCards.Raise();
     }
 
     private void GetExchangeCards()
     {
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 3; i++)
         {
             int cardPosition = Random.Range(0, deck.Cards.Count);
             exchangeCards.Add(deck.Cards[cardPosition]);
